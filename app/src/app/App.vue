243063c87eddb01cc4d007e80853400e3d3f36b0
judith lourde <judith.lourde@gmail.com>
2022-04-30 13:50:16 +0200

    Add navbar and routerview to the app

---
+++ app/src/app/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<section class="app">
+		<Navbar/>
+		<RouterView />
+	</section>
+	
+</template>
+
+<script>
+	import Navbar from '../components/Navbar.vue';
+	
+	export default {
+		components: { Navbar }
+	}
+</script>
+
+<style>
+	@import '../style/reset.css';
+	@import '../style/variables.css';
+	@import '../style/fonts.css';
+	@import '../style/style.css';
+</style>
