4aa2d7850e1238d57b546a10647fc73c9f09e4eb
judith lourde <judith.lourde@gmail.com>
2022-04-30 13:52:46 +0200

    Add ticket owner for the bug

---
+++ app/src/components/TicketOwnerDisplay.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="TicketOwnerDisplay-container">
+        <div class="TicketOwnerDisplay-container__developer-image">
+            <img v-if="ticketOwner" :src="ticketOwner" alt="">
+            <img v-else src="" alt="">
+        </div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            ticketOwner: { String }
+        }
+    }
+</script>
+
+<style>
+    .TicketOwnerDisplay-container {
+        width: 80%;
+        display: flex;
+        justify-content: center;
+    }
+
+    .TicketOwnerDisplay-container__developer-image {
+        width: 50px;
+        height: 50px;
+        border-radius: 25px;
+        overflow: hidden;
+    }
+
+    .TicketOwnerDisplay-container__developer-image img {
+        width: 100%;
+    }
+</style>
