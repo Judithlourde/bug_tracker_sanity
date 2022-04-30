5bf54bfaacdb1129f564af0385263f9fbedcf80b
judith lourde <judith.lourde@gmail.com>
2022-04-30 13:50:57 +0200

    Add navbar

---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+	<nav>
+		<div class="logo-container">
+			<img src="/images/monday_logo_icon.png" alt="Logo for the friday.com">
+		</div>
+		<div class="controls-container">
+			<div class="icon" >
+				<router-link :to="{ name:'ticket' }">
+					<img src="/svg/plus-icon.svg" alt="plus icon for make tickets">
+				</router-link>
+			</div>
+			<div class="icon">
+				<router-link :to="{ name:'dashboard' }">
+					<img src="/svg/back-arrow.svg" alt="arrow icon for going back to dashboard">
+				</router-link>
+			</div>
+		</div>
+	</nav>
+
+</template>
+
+<style>
+	nav {
+		height: 100vh;
+		/* width: 40%; */
+		background-color: #292F4C;
+		color: white;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	nav .logo-container,
+	nav .controls-container {
+		padding: 40px 25px;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.logo-container {
+		width: 100px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.logo-container img {
+		width: 110%;
+	}
+
+	.controls-container div a img {
+		width: 20px;
+		color: white;
+		padding: 5px 0;
+	}
+</style>
