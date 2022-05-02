2bb171da8feb047f2ae823bbfd4b08449dc2b419
judith lourde <judith.lourde@gmail.com>
2022-05-03 00:05:16 +0200

    Add submit date to the ticket card

---
+++ app/src/components/SubmitDateDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="submitDateDisplay-container">{{ submitDate }}</div>
+</template>
+
+<script>
+    export default {
+        props: {
+            submitDate: { String }
+        }
+    }
+</script>
+
+<style>
+    .submitDateDisplay-container {
+        width: 80%;
+        display: flex;
+        justify-content: center;
+        font-size: 16px;
+    }
+</style>
