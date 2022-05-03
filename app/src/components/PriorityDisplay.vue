52b5ad797c84b44a0f770bf13e75344d9c2363da
judith lourde <judith.lourde@gmail.com>
2022-05-03 23:26:12 +0200

    Fix priority background color

--- app/src/components/PriorityDisplay.vue
+++ app/src/components/PriorityDisplay.vue
@@ -1,20 +1,50 @@
 <template>
-    <div class="priority-container">
-        <div class="priority-container__stars">
+    <div class="priority-container" :style="{ backgroundColor: color }">{{ priority }}</div>
+    
+        <!-- <div class="priority-container__stars"> -->
             <!-- Set the stars count depending upon the priority cont -->
-            <h3 v-if="priority >=1" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3>
+            <!-- <h3 v-if="priority >=1" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3>
             <h3 v-if="priority >=2" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3>
             <h3 v-if="priority >=3" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3>
             <h3 v-if="priority >=4" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3>
-            <h3 v-if="priority >=5" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3>
-        </div>
-    </div>
+            <h3 v-if="priority >=5" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3> -->
+            
+
+        <!-- </div> -->
 </template>
 
 <script>
     export default {
         props: {
-            priority: { Number }
+            priority: { String }
+        },
+
+        created() {
+            this.getColor();
+        },
+
+        data() {
+            return {
+                color: ''
+            }
+        },
+
+        methods: {
+            getColor() {
+                switch (this.priority) {
+                    case 'critical':
+                        this.color = 'rgba(226, 67, 92, 1)'
+                        break
+                    case 'major':
+                        this.color = 'rgba(0, 200, 117, 1)'
+                        break
+                    case 'low':
+                        this.color = 'rgba(255, 203, 0, 1)'
+                        break
+                    default:
+                        this.color = 'rgba(86, 155, 253, 1)'
+                }
+            }
         }
     }
 </script>
@@ -23,6 +53,7 @@
     .priority-container {
         display: flex;
         justify-content: center;
+        font-size: 16px;
     }
 
     .priority-container__stars {
