78471deceefc2f672599019b3d61e9aeb42a3717
judith lourde <judith.lourde@gmail.com>
2022-05-04 23:27:30 +0200

    Fix formating code

--- app/src/views/TicketPage.vue
+++ app/src/views/TicketPage.vue
@@ -10,124 +10,36 @@
 				<form @submit.prevent="handleSubmit">
 					<section>
 						<label for="title">Title</label>
-						<input 
-							id="title" 
-							name="title" 
-							type="text" 
-							
-							v-model="formData.title"
-						/>
+						<input id="title" name="title" type="text" 	v-model="formData.title"/>
 
-						<!-- @keyup.enter="handleChange" -->
-
-						<label for="description">Description</label>
-						<input 
-							id="description" 
-							name="description" 
-							type="text" 
-							
-							v-model="formData.description"
-						/>
+						<label for="description">Description</label> 
+                    	<textarea id="description" name="description" rows="3" cols="20" type="text" v-model="formData.description"></textarea>
 
 						<label for="team">Team</label>
-						<select 
-							id="team"
-							name="team" 
-							v-model="formData.team"
-								
-						>
-							<!-- <div v-for="team in teams" :key="team.id"> -->
-								<option v-for="team in teams" :key="team.id" value="team">{{ team }}</option>
-							<!-- </div> -->
+						<select id="team" name="team" v-model="formData.team">
+							<option v-for="team in teams" :key="team.id" :value="team">{{ team }}</option>
 						</select>
 
 						<label for="new-team">New Team</label>
-						<input 
-							id="new-team" 
-							name="team" 
-							type="text" 
-							 
-							v-model="formData.team"
-						>
+						<input id="new-team" name="team" type="text" v-model="formData.team">
 
 						<label for="priority">Priority</label>
-						<div class="ticket__container-multiple-input-container">
-							<input 
-								id="priority-1"
-								name="priority"
-								type="radio"
-								
-								:value=1
-								v-model="formData.priority"
-							> 
-							<label for="priority-1">1</label>
+						<select id="priority" name="priority" v-model="formData.priority">
+							<option value="major">Major</option>
+							<option value="low">Low</option>
+							<option value="critical">Critical</option>
+						</select>
 
-								<input 
-								id="priority-2"
-								name="priority"
-								type="radio"
-								
-								:value=2
-								v-model="formData.priority"
-							> 
-							<label for="priority-2">2</label>
-
-								<input 
-								id="priority-3"
-								name="priority"
-								type="radio"
-								
-								:value=3
-								v-model="formData.priority"
-							> 
-							<label for="priority-3">3</label>
-
-								<input 
-								id="priority-4"
-								name="priority"
-								type="radio"
-								
-								:value=4
-								v-model="formData.priority"
-							> 
-							<label for="priority-4">4</label>
-
-								<input 
-								id="priority-5"
-								name="priority"
-								type="radio"
-								
-								:value=5
-								v-model="formData.priority"
-							> 
-							<label for="priority-5">5</label>
-						</div>
-
-						<!-- <div v-if="editMode"> -->
 						<div>
 							<label for="progress">Progress</label>
-							<input 
-								type="range"
-								id="progress"
-								name="progress"
-								:value="formData.progress"
-								min=0
-								max=100
-							>
+							<input type="range" id="progress" name="progress" v-model="formData.progress" min="0" max="100">
 							
 							<label for="status">Status</label>
-							<select 
-								id="status"
-								name="status" 
-								
-								v-model="formData.status"
-							
-							>
+							<select id="status" name="status" v-model="formData.status">
 								<option value="not started">Not started yet</option>
 								<option value="working on it">Working on it</option>
 								<option value="stuck">Stuck</option>
 								<option value="done">Done</option>
-								
 							</select>
 						</div>
 
@@ -136,48 +48,27 @@
 
 					<section>
 						<label for="reporter">Reporter</label>
-						<input 
-							id="reporter" 
-							name="reporter" 
-							type="text" 
-							 
-							v-model="formData.reporter"
-						>
+						<input id="reporter" name="reporter" type="text" v-model="formData.reporter">
 
 						<label for="reporterImage">Reporter Image</label>
-						<input 
-							id="reporterImage" 
-							name="reporterImage" 
-							type="text" 
-							 
-							v-model="formData.reporterImage"
-						>
+						<input id="reporterImage" name="reporterImage" type="text" v-model="formData.reporterImage">
 
-						<div class="reporter-image">
+						<!-- <div class="reporter-image">
 							<img v-if="formData.reporterImage" :src="formData.reporterImage" alt="Reporter image">
-						</div>
+						</div> -->
 
 						<label for="assignee">Assignee</label>
-						<input 
-							id="assignee" 
-							name="assignee" 
-							type="text" 
-							 
-							v-model="formData.assignee"
-						>
+						<input id="assignee" name="assignee" type="text" v-model="formData.assignee">
 
 						<label for="assigneeImage">Assignee Image</label>
-						<input 
-							id="assigneeImage" 
-							name="assigneeImage" 
-							type="text" 
-							 
-							v-model="formData.assigneeImage"
-						>
+						<input id="assigneeImage" name="assigneeImage" type="text" v-model="formData.assigneeImage">
 
-						<div class="assignee-image">
+						<!-- <div class="assignee-image">
 							<img v-if="formData.assigneeImage" :src="formData.assigneeImage" alt="Assignee image">
-						</div>
+						</div> -->
+
+						<label for="submitDate">Submit Date:</label>
+  						<input type="date" id="submitDate" name="submitDate" v-model="submitDate">
 					</section>
 				</form>
 			</div>
@@ -200,27 +91,18 @@
 				formData: {
 					title:'',
 					description: '',
-					priority: 0,
+					priority: '',
 					status: 'not started',
-					progress: 0,
-					timestamp: new Date().toISOString(),
+					progress: 50,
+					// timestamp: new Date().toISOString(),
+					submitDate: '',
 					team: '',
+					value: 10,
 				},
-				teams:[]
+				teams:[],
+				id: '',
 			}
 		},
-
-		// async created() {
-		// 	await this.sanityFetch(query, { 
-		// 		slug: this.$route.params.projectSlug
-		// 	});
-
-		// 	this.metaTags({
-		// 		title: this.result.title,
-		// 		description: this.result.description,
-		// 		image: this.result.documentation[0].asset.url
-		// 	});
-		// },
 
 		async created() {
 			await this.sanityFetch(query, { 
@@ -231,57 +113,67 @@
 		},
 
 		methods: {
-			handleSubmit() {
-				console.log('submitted')
-				sanity.create({
-					_type: 'bug',
-					title: this.formData.title,
-					description: this.formData.description,
-					priority: this.formData.priority,
-					status: this.formData.status,
-					progress: this.formData.progress,
-					submitDate: this.formData.timestamp,
-					team: this.formData.team,
+				handleSubmit() {
+					console.log('submitted')
+					sanity.create({
+						_type: 'bug',
+						// _id: 'my id',
+						title: this.formData.title,
+						description: this.formData.description,
+						priority: this.formData.priority,
+						status: this.formData.status,
+						progress: this.formData.progress,
+						submitDate: this.formData.submitDate,
+						// team: this.createTeam(this.id),
+					}).then((res) => {
+  						console.log(`Bike was created, document ID is ${res._id}`)
+						this.createTeam(res._id)
+					})
+					console.log(this.formData.team)
 
-				});
-			},
+				},
 
-			handleChange() {
-				console.log(this.formData.title)
-				console.log(this.formData.description)
-				console.log(this.formData.timestamp)
-				console.log(this.formData.status)
-				console.log(this.formData.category)
-				console.log(this.formData.priority)
-			},
+				createTeam(id) {
+					const doc = {
+						_id: 'id',
+						_type: 'team',
+						name: 'Sanity Tandem Extraordinaire',
+						// seats: 2,
+						}
+
+						sanity.createIfNotExists(doc).then((res) => {
+						console.log('Bike was created (or was already present)')
+					})
+				},
+
+				
+			
+
+			// createTeam(newTeam) {
+
+			// 	// sjekker om eksistere team
+			// 	const teams = sanity.getItem('team');
+			// 	const checkExistingTeam = teams.includes(newTeam);
+			// 	// hvis eksistere return med en gang
+			// 	if(checkExistingTeam) {
+			// 		return newTeam;
+			// 	} else {
+			// 		sanity.create('team', newTeam)
+			// 	}
+			// 	// hvis ikke eksistere
+			// 	// lager vi ny team i referanse felt
+			// 	// så returnere vi ny referanse
+
+			// 	return newTeam;
+			// },
+
+			
 
 			uniqueTickets() {
 				// Javascript Sets: https://alligator.io/js/sets-introduction/#:~:text=Sets%20are%20a%20new%20object,like%20object%20literals%20or%20arrays.
 				this.teams = [ ...new Set(this.result.map(({ team }) => team.name)) ];  // Change this.tickets to tickets? after getting data from database
 				console.log(this.uniqueTeams)
 			},
-
-			// patchProject() {
-			// 	/* bare hvis withCredentials: true! */
-
-			// 	sanity
-			// 		.patch('a99b7992-0974-4a7e-8ddb-6891f7a6776a')
-			// 		.set({ year: 2022 })
-			// 		.commit()
-			// 		.then(updatedDocument => {
-			// 			console.log('I just updated document:', updatedDocument);
-			// 		});
-			// },
-
-			// createProject() {
-			// 	/* bare hvis withCredentials: true! */
-
-			// 	sanity.create({
-			// 		_type: 'project',
-			// 		title: 'NEW PROJECT!!!',
-			// 	});
-			// },
-			
 		}
 	}
 </script>
