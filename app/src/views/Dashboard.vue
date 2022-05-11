0e03d3bcdbbf5ae314e24703eae94d925a7f9aba
judith lourde <judith.lourde@gmail.com>
2022-05-11 14:01:02 +0200

    Add project sorting in Dashboard

--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -3,31 +3,31 @@
 		<div v-if="loading">...</div>
 		<div v-else class="dashboard">
 			<h1>Bugs Tracker</h1>
-			<div class="dashboard__ticket-container">
-				<div v-for="(uniqueTeam, index) in uniqueTeams" :key="uniqueTeam.id"> 
+			<div class="dashboard__project-container">
+				<div v-for="(uniqueProject, index) in uniqueProjects" :key="uniqueProject.id"> 
 					<!-- {{ uniqueTeam }}  -->
 
-					<div class="dashboard__ticket-container-title">
-						<div :style="{ color: this.colors[index] || this.colors[0] }">{{ uniqueTeam }}</div>
+					<div class="dashboard__project-container-title">
+						<div :style="{ color: this.colors[index] || this.colors[0] }">{{ uniqueProject }}</div>
 						<div>Reporter</div> 
 						<div>Status</div>	
 						<div>Priority</div>
 						<div>Assignee</div>
 						<div>Progress </div>
-						<div>Submit Date</div>
+						<div>Due Date</div>
 					</div>
 
-					<div v-for="bug in result.filter(bug => bug.team.name === uniqueTeam)" :key="bug.id">
+					<div v-for="bug in result.filter(bug => bug.project.name === uniqueProject)" :key="bug.id">
 						<TicketCard 
-							:filteredTicket="bug"
+							:filteredBug="bug"
 							:color="this.colors[index] || this.colors[0]"
 						/>	
 					</div>	
 				</div>
 
-				<TicketPage 
-					:teams="this.uniqueTeams"
-				/>
+				<!-- <TicketPage 
+					:projects="this.uniqueProjects"
+				/> -->
 			</div>
 		</div>
 	</section>
@@ -37,13 +37,13 @@
 	import query from '../groq/dashboard.groq?raw';
 	import viewMixin from '../mixins/viewMixin.js';
 	import TicketCard from '../components/TicketCard.vue';
-	import TicketPage from '../views/TicketPage.vue';
+	// import TicketPage from '../views/TicketPage.vue';
 	export default {
 		mixins: [viewMixin],
 		data() {
 			return {
-				uniqueTeams:[],
-				filteredTickets:[],
+				uniqueProjects:[],
+				// filteredTickets:[],
 				colors:[
 							'rgba(162, 93, 220, 1)',
 							'rgba(0, 200, 117, 1)',
@@ -62,25 +62,18 @@
 			this.metaTags({
 				title: 'Bugs Tracker',
 			})
-			this.uniqueTickets();
+			this.projects();
 		},
 		
 		components: {
 			TicketCard,
 		},
 
-		computed: {
-			answer() {
-				console.log(this.category);
-			}
-			
-		},
-
 		methods: {
-			uniqueTickets() {
+			projects() {
 				// Javascript Sets: https://alligator.io/js/sets-introduction/#:~:text=Sets%20are%20a%20new%20object,like%20object%20literals%20or%20arrays.
-				this.uniqueTeams = [ ...new Set(this.result.map(({ team }) => team.name)) ]; 
-				console.log(this.uniqueTeams)
+				this.uniqueProjects = [ ...new Set(this.result.map(({ project }) => project.name)) ]; 
+				console.log(this.uniqueProjects)
 			},
 			
 		},	
@@ -94,20 +87,20 @@
 		width: 100%;
 	}
 
-	.dashboard__ticket-container {
+	.dashboard__project-container {
 		width: 100%;
 		height: 80vh;
 		/* overflow: scroll; */
 	}
 
-	.dashboard__ticket-container-title {
+	.dashboard__project-container-title {
 		width: 88vw;
 		display: flex;
 		font-size: 16px;
 		margin: 20px 0 0 0;
 	}
 
-	.dashboard__ticket-container-title > * {
+	.dashboard__project-container-title > * {
         margin: 2px;
         padding: 5px 0 0 20px ;
         width: 100%;
