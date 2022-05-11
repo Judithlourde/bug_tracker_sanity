9a0daa45d732b4931437cd511dd3a0aad3c77881
judith lourde <judith.lourde@gmail.com>
2022-05-11 23:52:22 +0200

    Add Functionality to add new bug

--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -5,16 +5,24 @@
 			<h1>Bugs Tracker</h1>
 			<div class="dashboard__project-container">
 				<div v-for="(uniqueProject, index) in uniqueProjects" :key="uniqueProject.id"> 
-					<!-- {{ uniqueTeam }}  -->
 
 					<div class="dashboard__project-container-title">
-						<div :style="{ color: this.colors[index] || this.colors[0] }">{{ uniqueProject }}</div>
-						<div>Reporter</div> 
-						<div>Status</div>	
-						<div>Priority</div>
-						<div>Assignee</div>
-						<div>Progress </div>
-						<div>Due Date</div>
+						<div>
+							<div class="ticketCard__color" :style="{ backgroundColor: this.colors[index] || this.colors[0] }"></div>
+							<div :style="{ color: this.colors[index] || this.colors[0] }">{{ uniqueProject }}</div>
+							<div>Reporter</div> 
+							<div>Status</div>	
+							<div>Priority</div>
+							<div>Assignee</div>
+
+							<div class="progress-display">
+								<div>
+									<div>Progress</div>
+								</div>
+							</div>
+
+							<div>Due Date</div>
+						</div>	
 					</div>
 
 					<div v-for="bug in result.filter(bug => bug.project.name === uniqueProject)" :key="bug.id">
@@ -23,7 +31,36 @@
 							:color="this.colors[index] || this.colors[0]"
 						/>	
 					</div>	
-				</div>
+
+					<div class="dashboard__project-container-newBug">
+						<div>
+							<div class="ticketCard__color" :style="{ backgroundColor: this.colors[index] || this.colors[0] }"></div>
+							
+							<div>
+								<div class="edit">
+									<div v-if="!edit" @click="addNewBug()">+ Add bug</div>
+									
+									<div v-else>
+										<input type="text" v-model="formData.title" @keyup.enter="createBug(uniqueProject)" placeholder="+ Add Bug">
+										<div>Add</div>
+									</div>
+								</div>
+							</div>
+							<div>Reporter</div> 
+							<div>Status</div>	
+							<div>Priority</div>
+							<div>Assignee</div>
+
+							<div class="progress-display">
+								<div>
+            						<div></div>
+        						</div>
+							</div>
+							
+							<div>Due Date</div>
+						</div>
+					</div>
+				</div> 
 
 				<!-- <TicketPage 
 					:projects="this.uniqueProjects"
@@ -34,6 +71,7 @@
 </template>
 
 <script>
+	import sanity from '../sanity.js';
 	import query from '../groq/dashboard.groq?raw';
 	import viewMixin from '../mixins/viewMixin.js';
 	import TicketCard from '../components/TicketCard.vue';
@@ -43,14 +81,27 @@
 		data() {
 			return {
 				uniqueProjects:[],
+				edit: false,
+				formData: {
+					title:'',
+					description: '',
+					priority: '',
+					status: '',
+					progress: 0,
+					submitDate: '',
+					team: '',
+					reporter: '',
+					assignee: '',
+				},
+				projectID: '',
 				// filteredTickets:[],
-				colors:[
-							'rgba(162, 93, 220, 1)',
-							'rgba(0, 200, 117, 1)',
-							'rgba(253, 172, 61, 1)',
-							'rgba(226, 67, 92, 1)',
-							'rgba(86, 155, 253, 1)',
-						]
+				colors: [
+					'rgba(162, 93, 220, 1)',
+					'rgba(0, 200, 117, 1)',
+					'rgba(253, 172, 61, 1)',
+					'rgba(226, 67, 92, 1)',
+					'rgba(86, 155, 253, 1)',
+				]
 			}
 		},
 
@@ -74,8 +125,41 @@
 				// Javascript Sets: https://alligator.io/js/sets-introduction/#:~:text=Sets%20are%20a%20new%20object,like%20object%20literals%20or%20arrays.
 				this.uniqueProjects = [ ...new Set(this.result.map(({ project }) => project.name)) ]; 
 				console.log(this.uniqueProjects)
+			},	
+
+			addNewBug() {
+				this.edit = !this.edit;
 			},
-			
+
+			createBug(uniqueProject) {
+				consolo.log(uniqueProject)
+				this.projectID = this.result.find(project => project.name === uniqueProject );
+				const project = {
+						_id: this.projectID._id,
+						_type: 'project',
+						name: uniqueProject,
+					}
+				sanity.createIfNotExists(project).then((res) => {
+					this.projectID = res._id;
+					this.test();
+				});
+			},
+
+			test() {
+				sanity.create({
+					_type: 'bug',
+					title: this.formData.title,
+					project: {
+						_type: 'reference',
+						_ref: this.projectID._id,
+					}
+				})
+				
+				.then(result => {
+					console.log(`Created book with id: ${result._id}`)
+				});
+			}
+		
 		},	
 	}
 	
@@ -83,7 +167,7 @@
 
 <style>
 	.dashboard {
-		padding: 30px;
+		padding: 20px;
 		width: 100%;
 	}
 
@@ -93,20 +177,35 @@
 		/* overflow: scroll; */
 	}
 
-	.dashboard__project-container-title {
-		width: 88vw;
+	.dashboard__project-container-title,
+	.dashboard__project-container-newBug {
 		display: flex;
-		font-size: 16px;
+        width: 90vw;
+	}
+
+	.dashboard__project-container-title div {
 		margin: 20px 0 0 0;
 	}
 
-	.dashboard__project-container-title > * {
-        margin: 2px;
-        padding: 5px 0 0 20px ;
-        width: 100%;
-        display: flex;
-        align-items: center;
-		
+	.dashboard__project-container-title div,
+	.dashboard__project-container-newBug div {
+		width: 100%;
+		display: flex;
+		font-size: 16px;
 	}
 
+	.dashboard__project-container-title div > *,
+	.dashboard__project-container-newBug div > * {
+		background-color: rgb(245, 245, 245);
+        margin: 1px;
+        padding: 5px;
+        width: 100%;
+        display: flex;
+		justify-content: center;
+        align-items: center;	
+	}
+
+	.dashboard__project-container-newBug div > * {
+		padding: 8px;
+	}
 </style>
