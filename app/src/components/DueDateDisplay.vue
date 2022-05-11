e892ae292e0257c1209b6b91b48f9184e0774b14
judith lourde <judith.lourde@gmail.com>
2022-05-11 13:58:53 +0200

    Change submit date to due date

---
+++ app/src/components/DueDateDisplay.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="dueDate" class="dueDateDisplay-container">{{ dueDate }}
+        <div v-if="!dueDate">Date</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            dueDate: { String }
+        }
+    }
+</script>
+
+<style>
+    .dueDateDisplay-container {
+        width: 80%;
+        display: flex;
+        justify-content: center;
+        font-size: 16px;
+    }
+</style>
