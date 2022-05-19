02b4923279b69467d17dd423bc79848881fc392b
judith lourde <judith.lourde@gmail.com>
2022-05-19 17:30:28 +0200

    Fix edit bug functionality

--- app/src/components/Ticket.vue
+++ app/src/components/Ticket.vue
@@ -1,71 +1,47 @@
 <template>
-	<section>
+	<section class="test">
 		<div v-if="loading">...</div> 
 		<div v-else class="ticket" v-for="bug in result" :key="bug._id"> 
-			<h1>{{ bug.title }}</h1>
-			
 			<div class="ticket__container">
+				<h1>{{ bug.title }}</h1>
 				<form>
 					<section>
                         <label for="reporter">Reporter</label>
-                        <input v-if="bug.reporter !== null " id="reporter" name="reporter" type="text" v-model="bug.reporter.name">
-						<input v-else id="reporter" name="reporter" type="text" v-model="bugData.reporter">
+						<input id="reporter" name="reporter" type="text" v-model="bugData.reporter">
 
                         <label for="assignee">Assignee</label>
-						<select v-if="bug.assignee !== null" id="assignee" name="assignee" v-model="bug.assignee.name">
-							<option v-for="projectMember in bug.project.projectMembers" :key="projectMember._id" :value="projectMember.name">{{ `${projectMember.name}` }}</option>
-						</select>
-
-                        <select v-else id="assignee" name="assignee" v-model="bugData.assignee">
+						<select id="assignee" name="assignee" v-model="bugData.assignee">
 							<option v-for="projectMember in bug.project.projectMembers" :key="projectMember._id" :value="projectMember.name">{{ `${projectMember.name}` }}</option>
 						</select>
 
                         <label for="dueDate">Due Date:</label>
-						<input v-if="bug.dueDate" type="date" id="dueData" name="dueDate" v-model="bug.date">
-                        <input v-else type="date" id="dueDate" name="dueDate" v-model="bugData.dueDate">
+						<input type="date" id="dueDate" name="dueDate" v-model="bugData.dueDate">
 
                         <label for="description">Description</label> 
-                    	<textarea v-if="bug.description" id="description" name="description" rows="4" cols="20" type="text" v-model="bug.description"></textarea>
-						<textarea v-else id="description" name="description" rows="4" cols="20" type="text" v-model="bugData.description"></textarea>
+						<textarea id="description" name="description" rows="4" cols="20" type="text" v-model="bugData.description"></textarea>
 
                         <!-- <label for="screenshot">Screenshot</label>
                         <input type="text" id="screenshot" name="screenshot" v-model="bugData.screenshot"> -->
 
 						<label for="priority">Priority</label>
-						<select v-if="bug.priority" id="priority" name="priority" v-model="bug.priority">
+						<select id="priority" name="priority" v-model="bugData.priority">
 							<option value="high">High</option>
 							<option value="medium">Medium</option>
 							<option value="low">Low</option>
 							<option value="critical">Critical</option>
 						</select>
-
-						<select v-else id="priority" name="priority" v-model="bugData.priority">
-							<option value="high">High</option>
-							<option value="medium">Medium</option>
-							<option value="low">Low</option>
-							<option value="critical">Critical</option>
-						</select>
-
-							<!-- <label for="progress">Progress</label> -->
-							<!-- <input type="range" id="progress" name="progress" v-model.number="bug.progress" min="0" max=100> -->
 							
 						<label for="status">Status</label>
-						<select v-if="bug.status" id="status" name="status" v-model="bug.status">
-							<option value="not started yet">Not started yet</option>
-							<option value="working on it">Working on it</option>
-							<option value="stuck">Stuck</option>
-							<option value="done">Done</option>
-						</select>
-
-						<select v-else id="status" name="status" v-model="bugData.status">
+						<select id="status" name="status" v-model="bugData.status">
 							<option value="not started yet">Not started yet</option>
 							<option value="working on it">Working on it</option>
 							<option value="stuck">Stuck</option>
 							<option value="done">Done</option>
 						</select>
                             
-						
-                        <input type="submit" @click.prevent="handleSubmit">
+						<router-link :to="{ name:'dashboard' }">
+                        	<input type="submit" @click.prevent="handleSubmit"> 
+						</router-link>
 					</section>
 				</form>
 			</div>
@@ -89,11 +65,8 @@
 					dueDate: '',
 					reporter: '',
 					assignee: '',
-                    screenshot: ''
 				},
-				reporterID: '',
-				assigneeID: '',
-				projectMemberID: '',
+				projectMember: '',
 			}
 		},
 
@@ -101,31 +74,53 @@
 			await this.sanityFetch(query, { 
 				slug: this.$route.params.ticketSlug 
 			});
+			this.handleBugData();
 		},
 
 		methods: {
-			handleSubmit() {
-				this.projectMemberID = this.result[0].project.projectMembers.find(member => member.name === this.bugData.assignee);
-				const assignee = {
-						_id: this.projectMemberID._id,
-						_type: 'employee',
-						name: this.bugData.assignee,
+			handleBugData() {
+				console.log(this.result[0].assignee.name)
+				this.result.map(bug => {
+					if(bug.reporter === null) {
+						this.bugData.reporter = ''
+					} else {
+						this.bugData.reporter = bug.reporter.name
 					}
-				sanity.createIfNotExists(assignee).then((res) => {
-					this.assigneeID = res._id;
-					this.createBug();
-				});
-
-				// console.log(this.result[0]._id)
-				// console.log(this.bugData.assignee)
+					
+					if(bug.assignee === null) {
+						this.bugData.assignee = ''
+					} else {
+						this.bugData.assignee = bug.assignee.name;
+					}
+					
+					this.bugData.dueDate = bug.dueDate;
+					this.bugData.description = bug.description;
+					this.bugData.priority = bug.priority;
+					this.bugData.status = bug.status;
+					
+				})
+				console.log(this.bugData)
+			},
+			handleSubmit() {
+				this.projectMember = this.result[0].project.projectMembers.find(member => member.name === this.bugData.assignee);
+					const assignee = {
+							_id: this.projectMember._id,
+							_type: 'employee',
+							name: this.bugData.assignee,
+						}
+					sanity.createIfNotExists(assignee).then((res) => {
+						this.assigneeID = res._id;
+						this.updateBug();
+					});
+					this.updateBug();
 			},
 
-			createBug() {
+			updateBug() {
 				sanity
 					.patch(this.result[0]._id)
 					.set({ assignee: {
 								_type: 'reference',
-								_ref: this.assigneeID,
+								_ref: this.projectMember._id,
 							}, 
 						})
 					.set({ description: this.bugData.description, })
@@ -134,7 +129,7 @@
 					.set({ dueDate: this.bugData.dueDate, })
 					.commit()
 					.then(updatedDocument => {
-						console.log('I just updated document:', updatedDocument);
+						// console.log('I just updated document:', updatedDocument);
 					});
 			}	
 		},
@@ -142,15 +137,51 @@
 </script>
 
 <style>
+	.test {
+		position: relative;
+		width: 80vw;
+		/* position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        object-fit: cover; */
+        /* width: 100vw; */
+        /* height: 100vh; */
+        /* z-index: 400; */
+	}
+
 	.ticket {
-		padding: 30px;
-		width: 100%;
+		position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        opacity: 0.8;
+        /* background-color: rgba(17, 17, 17, 0.7); */
+			/* padding: 30px; */
+			/* width: 100%; */
 	}
 
 	.ticket__container {
-		width: 100%;
+		/* width: 100%; */
 		display: flex;
 		justify-content: center;
+		transform: translateX(0);
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        display: flex;
+        flex-direction: column;
+        /* align-items: center; */
+        max-width: 500px;
+        height: 100%;
+        width: 100%;
+        background-color: #fff;
 	}
 
 	.ticket__container form {
