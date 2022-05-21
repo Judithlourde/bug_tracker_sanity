853b7cf670ff9327847c50323350f4ad8455afc7
judith lourde <judith.lourde@gmail.com>
2022-05-22 00:18:30 +0200

    Fix routes

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,26 +1,9 @@
-import Dashboard from './views/Dashboard.vue';
 import TicketPage from './views/TicketPage.vue';
 
 export default [
-	// { 
-	// 	name: 'projectPage', 
-	// 	path: '/:projectSlug',
-	// 	component: ProjectPage 
-	// },
-	
-	// { 
-	// 	name: 'ticketID', 
-	// 	path: '/ticket/:ticketID',
-	// 	component: TicketPage 
-	// },
 	{ 
 		name: 'ticketPage', 
 		path: '/:ticketSlug',
 		component: TicketPage 
-	},
-	{ 
-		name: 'dashboard', 
-		path: '/', 
-		component: Dashboard 
 	},
 ];
