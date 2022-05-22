5215f0edfdfb525912038149080476400f9f1395
judith lourde <judith.lourde@gmail.com>
2022-05-23 00:10:04 +0200

    Fix dynamic children routes

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,9 +1,32 @@
 import TicketPage from './views/TicketPage.vue';
+import Bug from './views/Bug.vue';
+import BugsBoard from './views/BugsBoard.vue';
+import LoginPage from './views/LoginPage.vue';
 
 export default [
 	{ 
-		name: 'ticketPage', 
-		path: '/:ticketSlug',
-		component: TicketPage 
+		name: 'bugsBoard', 
+		path: '/bugsBoard',
+		component: BugsBoard, 
+		children: [
+			{
+				name: 'bug', 
+				path: 'bug',
+				component: Bug,
+				children: [
+					{
+						name: 'ticketPage', 
+						path: ':ticketSlug',
+						component: TicketPage
+					}
+				]
+			},
+			
+		]
+	},
+	{ 
+		name: 'loginPage', 
+		path: '/',
+		component: LoginPage 
 	},
 ];
