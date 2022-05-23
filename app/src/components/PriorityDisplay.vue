0305365b77ce7bde0436ca16977f16e8e59a1897
judith lourde <judith.lourde@gmail.com>
2022-05-24 01:00:26 +0200

    Remove unwanted code

--- app/src/components/PriorityDisplay.vue
+++ app/src/components/PriorityDisplay.vue
@@ -1,17 +1,6 @@
 <template>
     <div v-if="priority" class="priority-container" :style="{ backgroundColor: color }">{{ priority }}</div>
     <div v-else></div>
-    
-        <!-- <div class="priority-container__stars"> -->
-            <!-- Set the stars count depending upon the priority cont -->
-            <!-- <h3 v-if="priority >=1" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3>
-            <h3 v-if="priority >=2" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3>
-            <h3 v-if="priority >=3" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3>
-            <h3 v-if="priority >=4" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3>
-            <h3 v-if="priority >=5" :style="{ color: 'rgba(162, 93, 220, 1)' }">☆</h3> -->
-            
-
-        <!-- </div> -->
 </template>
 
 <script>
