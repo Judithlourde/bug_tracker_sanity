0fb4688f44aaa7261d3afeea663e0b6701545255
judith lourde <judith.lourde@gmail.com>
2022-05-25 00:12:01 +0200

    Fix status background-color

--- app/src/components/StatusDisplay.vue
+++ app/src/components/StatusDisplay.vue
@@ -1,47 +1,34 @@
 <template>
-    <!-- <section> -->
-        <div v-if="bugStatus" class="status-container" :style="{backgroundColor: color}">{{ bugStatus }}</div>
+    <section class="status-container">
+        <div class="status-container__background-color" v-if="bugStatus === 'done'" :style="{backgroundColor: 'rgba(162, 93, 220, 1)'}">
+            <div>{{ bugStatus }}</div>
+        </div>
+
+        <div class="status-container__background-color" v-if="bugStatus === 'working on it'" :style="{backgroundColor: 'rgba(0, 200, 117, 1)'}">
+            <div>{{ bugStatus }}</div>
+        </div>
+
+        <div class="status-container__background-color" v-if="bugStatus === 'stuck'" :style="{backgroundColor: 'rgb(255,203,0)'}">
+            <div>{{ bugStatus }}</div>
+        </div>
+        <div class="status-container__background-color" v-if="bugStatus === 'not started yet'" :style="{backgroundColor: 'rgb(255,203,0)'}">
+            <div>{{ bugStatus }}</div>
+        </div>
         <div v-else></div>
-    <!-- </section> -->
+    </section>
 </template>
 
-<script>
+<script> 
     export default {
         props: {
-            bugStatus: { String }
-        },
-
-        created() {
-            this.getColor()
+            bugStatus: { String },
         },
 
         data() {
             return {
-                status: this.bugStatus,
-                color: '',
+            
             }
         },
-
-        methods: {
-            getColor() {
-                switch (this.status) {
-                    case 'done':
-                        this.color = 'rgba(162, 93, 220, 1)';
-                        break
-
-                    case 'working on it':
-                        this.color = 'rgba(0, 200, 117, 1)';
-                        break
-
-                    case 'stuck':
-                        this.color = 'rgb(255,203,0)';
-                        break
-
-                    default:
-                        this.color = 'rgba(86, 155, 253, 1)';
-                }
-            }
-        }
     }
 </script>
 
@@ -50,5 +37,14 @@
         display: flex;
         justify-content: center;
         font-size: 16px;
+    }
+
+    .status-container__background-color {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: white;
     }
 </style>
