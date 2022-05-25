259c85a3483211b30731889a88c9d4f6f1c867e9
judith lourde <judith.lourde@gmail.com>
2022-05-25 21:01:19 +0200

    test

--- app/src/sanity.js
+++ app/src/sanity.js
@@ -7,3 +7,18 @@ export default sanityClient({
 	useCdn: false, //false for localhost, true for netlify
 	withCredentials: true
 });
+
+// import sanityClient from '@sanity/client'
+
+// export const writeClient = sanityClient({
+//   projectId: 'some-project-id',
+//   dataset: 'my-dataset',
+//   token: process.env.SANITY_AUTH_TOKEN,
+//   useCdn: false
+// })
+
+// export const readClient = sanityClient({
+//   projectId: 'some-project-id',
+//   dataset: 'my-dataset',
+//   useCdn: true
+// })
