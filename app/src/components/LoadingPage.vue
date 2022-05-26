977ede47731e10ae3c5d0316fd5f17e5b1db160a
judith lourde <judith.lourde@gmail.com>
2022-05-27 00:10:21 +0200

    Add loading page animation

--- app/src/components/LoadingPage.vue
+++ app/src/components/LoadingPage.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="loading">
-        <p class="loading__title">Loading</p>
+        <p class="loading__title">Loading...</p>
 
         <div class="loading__container">
-            <img src="/svg/bugsTracker-logo.svg" alt="bugs tracker logo" class="loading__logo logo1">
+            <img src="/svg/bugsTracker-logo.svg" alt="bugs tracker logo" class="loading__logo logo">
         </div>
     </div>
 </template>
@@ -14,50 +14,60 @@
 </script>
 
 <style>
-.loading {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 40px;
-}
-.loading__title {
-    font-size: 4em;
-    padding: 20px;
-}
-.loading__container {
-    display: flex;
-    justify-content: center;
-}
-.loading__logo {
-    width: 30px;
-    height: 30px;
-    position: relative;
-    margin:  2% 25% 0% 25%;
-    animation: loading 2.5s ease-in-out infinite;
-}
-.logo1 {
-    animation-delay: 0.3s;
-}
-/* .logo2 {
-    animation-delay: 0.6s;
-}
-.logo3 {
-    animation-delay: 0.9s;
-} */
-@keyframes loading {
-    0%, 100%  {
-        transform: scale(0.2);
-        }
-    50%  {
-        transform: scale(1);
-       }
-    /* 100% {
-        transform: rotate(2turn);
-    } */
-}
+    .loading {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 40px;
+        animation: loading 2.5s ease-in-out infinite;
+    }
+    .loading__title {
+        font-size: 4em;
+        padding: 20px;
+       
+    }
+    .loading__container {
+        display: flex;
+        justify-content: center;
+    }
+    .loading__logo {
+        width: 30px;
+        height: 30px;
+        position: relative;
+        margin:  2% 25% 0% 25%;
+        
+    }
+    .logo {
+        
+        width: 100px;
+        
+    }
 
-
+    @keyframes loading {
+        0%{
+			transform: translateX(500px);
+		}
+		60%{
+			transform: translateX(0px);
+		}
+		62%{
+			transform: translateX(60px);
+		}
+		70%{
+			transform: skew(-10deg);
+		}
+		80%{
+			transform: skew(0deg) translateX(0px);
+		}
+		90%{
+			transform: skew(-20deg);
+		}
+		100%{
+			transform: skew(0deg);
+		}
+    
+    }
 </style>
