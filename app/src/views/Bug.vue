749a859b7c06a8f3c56cfd529aab74708d0b56b5
judith lourde <judith.lourde@gmail.com>
2022-05-27 00:11:45 +0200

    Add animation styling code

--- app/src/views/Bug.vue
+++ app/src/views/Bug.vue
@@ -1,44 +1,13 @@
 <template>
-    <section class="bug-layout">
+    <section class="bug-layout" >
         <RouterView />
     </section>
 </template>
 
 <script>
+    export default {
+    }
 </script>
 
 <style>
-    .bug-layout {
-        /* transform: translateX(0); */
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        display: flex;
-        flex-direction: column;
-        /* align-items: center; */
-        max-width: 500px;
-        height: 100%;
-        width: 100%;
-        background-color: #fff;
-    }
-
-    /* .slide-side-enter-from
-	{
-		opacity: 0;
-        transform: translateY(-250px);
-	}
-
-	.slide-side-leave-to {
-		opacity: 1;
-        transform: translateY(0);
-	}
-
-	.slide-side-enter-active {
-		animation: slide 3s ease-in;
-	}
-
-	.slide-side-leave-active {
-		animation: slide 3s ease-out;
-	} */
 </style>
